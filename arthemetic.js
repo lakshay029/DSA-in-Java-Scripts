ae1a69fc976d720bf6dc45da561b3682f6b01ecc
lakshay029 <126890120+lakshay029@users.noreply.github.com>
2026-01-23 13:51:27 +0530

    Add files via upload

---
+++ arthemetic.js
@@ -0,0 +1,20 @@
+// for(let i=0;i<10;){
+//     console.log(++i)
+// }
+
+// let a=5
+// let b= --a
+// console.log(--a)
+// console.log(b)
+
+// PEMDAS
+// perenthsis
+// exponantiate
+// multiplication and divsion
+// addtition and substraction
+
+// let num= 6**2 / 4+ 5-1
+// console.log(num)
+
+
+
